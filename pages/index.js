e5b2cc35050b3d0625e27181bd8b8ade951e6aa7
Leejinuk <leejin7900@gmail.com>
2022-09-26 12:41:53 +0900

    init5

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import ProductItem from "../components/ProductItem";
 export default function Home() {
   return (
     <Layout title="Home">
-      <div className="grid grid-cols1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
         {data.products.map((product) => (
           <ProductItem product={product} key={product.slug} />
         ))}
