0e170d759635d89cd50963b42dfce5bbeae64933
Leejinuk <leejin7900@gmail.com>
2022-09-26 12:59:03 +0900

    init6

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import data from "../utils/data";
-import Head from "next/head";
 import Layout from "../components/Layout";
 import ProductItem from "../components/ProductItem";
+import { React } from "react";
 
 export default function Home() {
   return (
