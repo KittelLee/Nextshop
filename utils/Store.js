c4911568a58708a17077bf0e0a87702091ab0574
Leejinuk <leejin7900@gmail.com>
2022-11-15 18:49:45 +0900

    init26

--- utils/Store.js
+++ utils/Store.js
@@ -1,13 +1,12 @@
-import Cookies from "js-cookie";
-import React from "react";
 import { createContext, useReducer } from "react";
+import Cookies from "js-cookie";
 
 export const Store = createContext();
 
 const initialState = {
   cart: Cookies.get("cart")
     ? JSON.parse(Cookies.get("cart"))
-    : { cartItems: [] },
+    : { cartItems: [], shippingAddress: {}, paymentMethod: "" },
 };
 
 function reducer(state, action) {
@@ -22,7 +21,6 @@ function reducer(state, action) {
             item.name === existItem.name ? newItem : item
           )
         : [...state.cart.cartItems, newItem];
-
       Cookies.set("cart", JSON.stringify({ ...state.cart, cartItems }));
       return { ...state, cart: { ...state.cart, cartItems } };
     }
@@ -30,7 +28,6 @@ function reducer(state, action) {
       const cartItems = state.cart.cartItems.filter(
         (item) => item.slug !== action.payload.slug
       );
-
       Cookies.set("cart", JSON.stringify({ ...state.cart, cartItems }));
       return { ...state, cart: { ...state.cart, cartItems } };
     }
@@ -41,6 +38,27 @@ function reducer(state, action) {
           cartItems: [],
           shippingAddress: { location: {} },
           paymentMethod: "",
+        },
+      };
+    case "CART_CLEAR_ITEMS":
+      return { ...state, cart: { ...state.cart, cartItems: [] } };
+    case "SAVE_SHIPPING_ADDRESS":
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: {
+            ...state.cart.shippingAddress,
+            ...action.payload,
+          },
+        },
+      };
+    case "SAVE_PAYMENT_METHOD":
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          paymentMethod: action.payload,
         },
       };
     default:
--- utils/db.js
+++ utils/db.js
@@ -19,6 +19,7 @@ async function connect() {
   console.log("new connection");
   connection.isConnected = db.connections[0].readyState;
 }
+
 async function disconnect() {
   if (connection.isConnected) {
     if (process.env.NODE_ENV === "production") {
@@ -29,5 +30,13 @@ async function disconnect() {
     }
   }
 }
-const db = { connect, disconnect };
+
+function convertDocToObj(doc) {
+  doc._id = doc._id.toString();
+  doc.createdAt = doc.createdAt.toString();
+  doc.updatedAt = doc.updatedAt.toString();
+  return doc;
+}
+
+const db = { connect, disconnect, convertDocToObj };
 export default db;
